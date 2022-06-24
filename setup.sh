1071dc8ce8d6f484ac53a1d89f184afd1e41a30f
fbartik <fbartik@osc.edu>
2022-06-24 15:15:37 -0400

    Tweaked dotconf alias in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-alias dotconf='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
 mkdir -p ${HOME}/.config-backup && \
-dotconf checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
+/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
 xargs -I{} mv {} ${HOME}/.config-backup/{}
 
-dotconf checkout
+/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout
 
-dotconf config --local status.showUntrackedFiles no
+/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME  config --local status.showUntrackedFiles no
