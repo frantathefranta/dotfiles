352af4f207d69743ffe1624d79b5ac4909357430
fbartik <fbartik@osc.edu>
2023-02-27 10:32:51 -0500

    Added taskrc file

--- dot_config/nvim/lua/plugins/utils.lua
+++ dot_config/nvim/lua/plugins/utils.lua
@@ -1,13 +1,14 @@
 return {
-  {
-    "telescope.nvim",
-    dependencies = {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-      config = function()
-        require("telescope").load_extension("fzf")
-      end,
-    },
-  },
-  "Pocco81/auto-save.nvim",
+	{
+		"telescope.nvim",
+		dependencies = {
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+			config = function()
+				require("telescope").load_extension("fzf")
+			end,
+		},
+	},
+	"Pocco81/auto-save.nvim",
+	"fladson/vim-kitty",
 }
