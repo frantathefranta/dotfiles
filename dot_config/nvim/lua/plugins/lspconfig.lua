70eaf26fb0272e0dfa9edec2a3fb022c2657c60e
fbartik <fbartik@osc.edu>
2023-03-01 12:40:23 -0500

    Added new treesitter parsers

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -1,20 +1,21 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
-    opts = {
-      ---@type lspconfig.options
-      servers = {
-        lua_ls = {},
-        pyright = {},
-        ansiblels = {},
-        yamlls = {},
-        dockerls = {},
-        rust_analyzer = {},
-        docker_compose_language_service = {},
-        awk_ls = {},
-        bashls = {},
-      },
-    },
-  },
+	{
+		"neovim/nvim-lspconfig",
+		---@class PluginLspOpts
+		opts = {
+			---@type lspconfig.options
+			servers = {
+				marksman = {},
+				lua_ls = {},
+				pyright = {},
+				ansiblels = {},
+				yamlls = {},
+				dockerls = {},
+				rust_analyzer = {},
+				docker_compose_language_service = {},
+				awk_ls = {},
+				bashls = {},
+			},
+		},
+	},
 }
