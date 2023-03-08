38cbb8372245712cf56ab9cf51b8d5e800ba4a5e
frantathefranta <frantabart@gmail.com>
2023-03-07 22:17:38 -0500

    Added .local/bin to PATH and added vim-wakatime to nvim config

--- dot_config/nvim/lua/plugins/utils.lua
+++ dot_config/nvim/lua/plugins/utils.lua
@@ -1,51 +1,52 @@
 return {
-	{ "echasnovski/mini.pairs", enabled = false },
-	{
-		"telescope.nvim",
-		dependencies = {
-			"nvim-telescope/telescope-fzf-native.nvim",
-			build = "make",
-			config = function()
-				require("telescope").load_extension("fzf")
-			end,
-		},
-	},
-	{
-		"Pocco81/auto-save.nvim",
-		event = "VeryLazy",
-	},
-	{
-		"fladson/vim-kitty",
-		ft = "kitty",
-	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = {
-			ensure_installed = {
-				"awk",
-				"comment",
-				"bash",
-				"dockerfile",
-				"gitcommit",
-				"gitignore",
-				"make",
-				"perl",
-				"terraform",
-				"toml",
-				"toml",
-				"help",
-				"json",
-				"lua",
-				"markdown",
-				"markdown_inline",
-				"python",
-				"query",
-				"regex",
-				"tsx",
-				"typescript",
-				"vim",
-				"yaml",
-			},
-		},
-	},
+  { "echasnovski/mini.pairs", enabled = false },
+  { "wakatime/vim-wakatime", event = "VeryLazy" },
+  {
+    "telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
+  },
+  {
+    "Pocco81/auto-save.nvim",
+    event = "VeryLazy",
+  },
+  {
+    "fladson/vim-kitty",
+    ft = "kitty",
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "awk",
+        "comment",
+        "bash",
+        "dockerfile",
+        "gitcommit",
+        "gitignore",
+        "make",
+        "perl",
+        "terraform",
+        "toml",
+        "toml",
+        "help",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
 }
