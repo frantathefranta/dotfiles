32872c1ec5de3286522abcefd76ca4260867d7b1
fbartik <fbartik@osc.edu>
2023-03-08 10:04:12 -0500

    Merging utils.lua

--- dot_config/nvim/lua/plugins/utils.lua
+++ dot_config/nvim/lua/plugins/utils.lua
@@ -1,4 +1,5 @@
 return {
+<<<<<<< Updated upstream
   { "echasnovski/mini.pairs", enabled = false },
   { "wakatime/vim-wakatime", event = "VeryLazy" },
   {
@@ -49,4 +50,56 @@ return {
       },
     },
   },
+=======
+	{ "echasnovski/mini.pairs", enabled = false },
+	{ "wakatime/vim-wakatime", event = "VeryLazy" },
+	{
+		"telescope.nvim",
+		dependencies = {
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+			config = function()
+				require("telescope").load_extension("fzf")
+			end,
+		},
+	},
+	{
+		"Pocco81/auto-save.nvim",
+		event = "VeryLazy",
+	},
+	{
+		"fladson/vim-kitty",
+		ft = "kitty",
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"awk",
+				"comment",
+				"bash",
+				"dockerfile",
+				"gitcommit",
+				"gitignore",
+				"make",
+				"perl",
+				"terraform",
+				"toml",
+				"toml",
+				"help",
+				"json",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"query",
+				"regex",
+				"tsx",
+				"typescript",
+				"vim",
+				"yaml",
+			},
+		},
+	},
+>>>>>>> Stashed changes
 }
