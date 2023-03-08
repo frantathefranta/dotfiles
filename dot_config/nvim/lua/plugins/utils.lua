fa0a6f3d659be192492ac5b3748daf336e5bd17c
fbartik <fbartik@osc.edu>
2023-03-08 10:10:15 -0500

    Added encrypted wakatime cfg + other changes to nvim config

--- dot_config/nvim/lua/plugins/utils.lua
+++ dot_config/nvim/lua/plugins/utils.lua
@@ -1,56 +1,4 @@
 return {
-<<<<<<< Updated upstream
-  { "echasnovski/mini.pairs", enabled = false },
-  { "wakatime/vim-wakatime", event = "VeryLazy" },
-  {
-    "telescope.nvim",
-    dependencies = {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-      config = function()
-        require("telescope").load_extension("fzf")
-      end,
-    },
-  },
-  {
-    "Pocco81/auto-save.nvim",
-    event = "VeryLazy",
-  },
-  {
-    "fladson/vim-kitty",
-    ft = "kitty",
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "awk",
-        "comment",
-        "bash",
-        "dockerfile",
-        "gitcommit",
-        "gitignore",
-        "make",
-        "perl",
-        "terraform",
-        "toml",
-        "toml",
-        "help",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "tsx",
-        "typescript",
-        "vim",
-        "yaml",
-      },
-    },
-  },
-=======
 	{ "echasnovski/mini.pairs", enabled = false },
 	{ "wakatime/vim-wakatime", event = "VeryLazy" },
 	{
@@ -101,5 +49,54 @@ return {
 			},
 		},
 	},
->>>>>>> Stashed changes
+	{ "echasnovski/mini.pairs", enabled = false },
+	{ "wakatime/vim-wakatime", event = "VeryLazy" },
+	{
+		"telescope.nvim",
+		dependencies = {
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+			config = function()
+				require("telescope").load_extension("fzf")
+			end,
+		},
+	},
+	{
+		"Pocco81/auto-save.nvim",
+		event = "VeryLazy",
+	},
+	{
+		"fladson/vim-kitty",
+		ft = "kitty",
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"awk",
+				"comment",
+				"bash",
+				"dockerfile",
+				"gitcommit",
+				"gitignore",
+				"make",
+				"perl",
+				"terraform",
+				"toml",
+				"toml",
+				"help",
+				"json",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"query",
+				"regex",
+				"tsx",
+				"typescript",
+				"vim",
+				"yaml",
+			},
+		},
+	},
 }
