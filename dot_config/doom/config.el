a167e3f02966a5e9604a12dee93968057ffa6259
frantathefranta <frantabart@gmail.com>
2023-04-16 22:58:14 -0400

    Added auto-refresh to emacs

--- dot_config/doom/config.el
+++ dot_config/doom/config.el
@@ -42,6 +42,9 @@
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/syncthing/org/")
 
+;; Auto revert (refresh actually, I don't understand the language here) files when they change
+;; Copied from here https://kundeveloper.com/blog/autorevert/
+(global-auto-revert-mode t)
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
