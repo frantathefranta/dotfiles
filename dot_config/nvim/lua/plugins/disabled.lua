a32f0fc420fd5e2faccb74eed41122d3e8796fb6
fbartik <fbartik@osc.edu>
2023-05-08 15:24:00 -0400

    Disabled some lazy plugins

--- dot_config/nvim/lua/plugins/disabled.lua
+++ dot_config/nvim/lua/plugins/disabled.lua
@@ -1,4 +1,14 @@
 return {
   { "folke/noice.nvim", enabled = false },
   { "echasnovski/mini.pairs", enabled = false },
+  { "akinsho/bufferline.nvim", enabled = false },
+  { "rafamadriz/friendly-snippets", enabled = false },
+  { "L3MON4D3/LuaSnip", enabled = false },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "folke/neoconf.nvim", enabled = false },
+  { "folke/neodev.nvim", enabled = false },
+  { "jose-elias-alvarez/null-ls.nvim", enabled = false },
+  -- { "nvim-pack/nvim-spectre", enabled = false },
+  { "windwp/nvim-spectre", enabled = false },
+  { "folke/which-key.nvim", enabled = false },
 }
