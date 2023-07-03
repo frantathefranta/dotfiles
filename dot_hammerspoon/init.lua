1ff613292aca46c69776f92a92cbecf7be2d50f1
fbartik <fbartik@osc.edu>
2023-07-03 11:29:27 -0400

    Update .hammerspoon/init.lua

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -1,39 +1,39 @@
 local hyper = {"cmd", "alt", "ctrl", "shift"}
 local editor = "Emacs"
 
-local quick_edit_app = nil
--- This block copies a text field in browser and opens it in Emacs
--- More information here https://github.com/meain/dotfiles/blob/25863934a8bcab3e1075cf2f7564b1290b77d14b/hammerspoon/.config/hammerspoon/init.lua#L363
-hs.hotkey.bind(
-    hyper,
-    "`",
-    function()
-        local emacs = hs.application.find(editor)
-        local current_app = hs.window.focusedWindow()
-        if current_app:title():sub(1, 5) == editor then
-            if quick_edit_app == nil then
-                hs.alert("🤔 No edit in progress")
-                return
-            end
-            hs.eventtap.keyStroke({"alt", "shift"}, ";")
-            hs.eventtap.keyStrokes("(meain/quick-edit-end)")
-            hs.eventtap.keyStroke({}, "return")
-            quick_edit_app:focus()
-            os.execute("sleep " .. tonumber(1))
-            hs.eventtap.keyStroke({"cmd"}, "a")
-            hs.eventtap.keyStroke({"cmd"}, "v")
-            quick_edit_app = nil
-        else
-            quick_edit_app = hs.window.focusedWindow()
-            hs.eventtap.keyStroke({"cmd"}, "a")
-            hs.eventtap.keyStroke({"cmd"}, "c")
-            emacs:activate()
-            hs.eventtap.keyStroke({"alt", "shift"}, ";")
-            hs.eventtap.keyStrokes("(meain/quick-edit)")
-            hs.eventtap.keyStroke({}, "return")
-        end
-    end
-)
+-- local quick_edit_app = nil
+-- -- This block copies a text field in browser and opens it in Emacs
+-- -- More information here https://github.com/meain/dotfiles/blob/25863934a8bcab3e1075cf2f7564b1290b77d14b/hammerspoon/.config/hammerspoon/init.lua#L363
+-- hs.hotkey.bind(
+--     hyper,
+--     "`",
+--     function()
+--         local emacs = hs.application.find(editor)
+--         local current_app = hs.window.focusedWindow()
+--         if current_app:title():sub(1, 5) == editor then
+--             if quick_edit_app == nil then
+--                 hs.alert("🤔 No edit in progress")
+--                 return
+--             end
+--             hs.eventtap.keyStroke({"alt", "shift"}, ";")
+--             hs.eventtap.keyStrokes("(meain/quick-edit-end)")
+--             hs.eventtap.keyStroke({}, "return")
+--             quick_edit_app:focus()
+--             os.execute("sleep " .. tonumber(1))
+--             hs.eventtap.keyStroke({"cmd"}, "a")
+--             hs.eventtap.keyStroke({"cmd"}, "v")
+--             quick_edit_app = nil
+--         else
+--             quick_edit_app = hs.window.focusedWindow()
+--             hs.eventtap.keyStroke({"cmd"}, "a")
+--             hs.eventtap.keyStroke({"cmd"}, "c")
+--             emacs:activate()
+--             hs.eventtap.keyStroke({"alt", "shift"}, ";")
+--             hs.eventtap.keyStrokes("(meain/quick-edit)")
+--             hs.eventtap.keyStroke({}, "return")
+--         end
+--     end
+-- )
 hs.hotkey.bind(hyper, "k", function()
 	hs.application.launchOrFocus("/Applications/iTerm.app")
 end)
