8da6386ff0497b85e6b5671741c605bd7ce81486
fbartik <fbartik@osc.edu>
2023-07-03 11:45:36 -0400

    Update .hammerspoon/init.lua

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -1,8 +1,8 @@
 local hyper = {"cmd", "alt", "ctrl", "shift"}
 local editor = "Emacs"
 
--- Optional configuration of beginEditShellCommand
-spoon.editWithEmacs.openEditorShellCommand = "EDITOR -e '(hammerspoon-edit-begin)'"
+-- -- Optional configuration of beginEditShellCommand
+-- spoon.editWithEmacs.openEditorShellCommand = "EDITOR -e '(hammerspoon-edit-begin)'"
 
 hs.loadSpoon("editWithEmacs")
 if spoon.editWithEmacs then
