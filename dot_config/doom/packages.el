44191c709ee508e977ac6688c1cd76c67534ff53
fbartik <fbartik@osc.edu>
2023-07-25 14:36:57 -0400

    Update .config/doom/packages.el

--- dot_config/doom/packages.el
+++ dot_config/doom/packages.el
@@ -13,6 +13,7 @@
 (package! mixed-pitch)
 (package! plz) ;; An HTTP library for Emacs
 (package! hass)
+(package! hackernews)
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/radian-software/straight.el#the-recipe-format
