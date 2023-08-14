11569d94ffd8f7c39528effbd58b9c9d402bdd95
fbartik <fbartik@osc.edu>
2023-08-14 11:35:26 -0400

    Update .config/nvim/lua/plugins/work_darwin_firenvim.lua

--- dot_config/nvim/lua/plugins/work_darwin_firenvim.lua
+++ dot_config/nvim/lua/plugins/work_darwin_firenvim.lua
@@ -1,12 +1,11 @@
 return {
 	{
 		"glacambre/firenvim",
-		cond = not not vim.g.started_by_firenvim,
-		build = function()
-			require("lazy").load({ plugins = "firenvim", wait = true })
-			vim.fn["firenvim#install"](0)
-		end,
-		-- Lazy load firenvim
-		-- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
+    -- Lazy load firenvim
+    -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
+    lazy = not vim.g.started_by_firenvim,
+    build = function()
+        vim.fn["firenvim#install"](0)
+    end
 	},
 }
