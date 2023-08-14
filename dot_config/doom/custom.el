f8037550fcbecbfd343d752692f4068ef838f247
fbartik <fbartik@osc.edu>
2023-08-14 11:36:23 -0400

    Update .config/doom/custom.el

--- dot_config/doom/custom.el
+++ dot_config/doom/custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" "6945dadc749ac5cbd47012cad836f92aea9ebec9f504d32fe89a956260773ca4" default))
  '(org-agenda-files
    '("/Users/fbartik/syncthing/org/agenda/home/house_reno.org" "/Users/fbartik/syncthing/org/agenda/work/work.org" "/Users/fbartik/syncthing/org/2.org"))
  '(package-selected-packages '(uuid)))
@@ -11,4 +13,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(ts-fold-replacement-face ((t (:foreground unspecified :box nil :inherit font-lock-comment-face :weight light)))))
