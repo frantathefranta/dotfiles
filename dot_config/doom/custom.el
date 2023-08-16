b33cbff3c94c3b4d3f952e8764b7c98c0d4a8f65
fbartik <fbartik@osc.edu>
2023-08-16 10:31:00 -0400

    Update .config/doom/custom.el

--- dot_config/doom/custom.el
+++ dot_config/doom/custom.el
@@ -5,8 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" "6945dadc749ac5cbd47012cad836f92aea9ebec9f504d32fe89a956260773ca4" default))
- '(org-agenda-files
-   '("/Users/fbartik/syncthing/org/agenda/home/house_reno.org" "/Users/fbartik/syncthing/org/agenda/work/work.org" "/Users/fbartik/syncthing/org/2.org"))
+ '(org-agenda-files '("/Users/fbartik/syncthing/org/inbox.org"))
  '(package-selected-packages '(uuid)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
