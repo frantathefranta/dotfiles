0689a9e0c6b496af039ec46dbdb4ab46104cd144
Franta Bartik <fb@franta.us>
2024-05-16 19:27:33 -0400

    Update .config/nvim/lua/plugins/yaml.lua

--- dot_config/nvim/lua/plugins/yaml.lua
+++ dot_config/nvim/lua/plugins/yaml.lua
@@ -124,4 +124,22 @@ return {
 			require("telescope").load_extension("yaml_schema")
 		end,
 	},
+	{
+		"nvim-lualine/lualine.nvim",
+		event = "VeryLazy",
+		opts = function(_, opts)
+			table.insert(opts.sections.lualine_x, {
+				function()
+					return require("yaml-companion").get_buf_schema(0)
+				end,
+				cond = function()
+					local schema = require("yaml-companion").get_buf_schema(0)
+					if schema.result[1].name == "none" then
+						return ""
+					end
+					return schema.result[1].name
+				end,
+			})
+		end,
+	},
 }
