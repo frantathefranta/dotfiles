25a12d1befcdf2faa38ad09f4bab1e9d4665ef50
Franta Bartik <fb@franta.us>
2024-05-16 19:46:15 -0400

    Update .config/nvim/lua/plugins/yaml.lua

--- dot_config/nvim/lua/plugins/yaml.lua
+++ dot_config/nvim/lua/plugins/yaml.lua
@@ -63,6 +63,7 @@ return {
 								["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
 								["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
 								["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+								["https://raw.githubusercontent.com/fluxcd-community/flux2-schemas/main/kustomization-kustomize-v1.json"] = "ks.{yml,yaml}",
 								["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
 								["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
 								["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
